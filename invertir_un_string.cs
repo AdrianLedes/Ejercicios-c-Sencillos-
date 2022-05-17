a599d6e6b4d9940cddb0398d41383bffe54e7d0c
AdrianLedes <72476604+AdrianLedes@users.noreply.github.com>
2022-05-16 22:53:45 -0500

    Add files via upload
    Pequeño ejerció que a veces puedes encontrarte en alguna prueba técnica para la posición de desarrollador/programador Jr.  Solo es invertir una cadena de String.

---
+++ invertir_un_string.cs
@@ -0,0 +1,17 @@
+﻿// Logra que la cadena de string sea leida correctamente invirtiendola.
+
+String text = "gnirts led anedac al ritrevni odargol zah euq se otxet etse reel sedeup is";
+
+//Creamos una varible para poder guardar la cenada a la hora de invertirla.
+String rest = "";
+
+//Hacemos un recorrido al string pero de manera inversa a lo que comun mente se hace.
+//se recorrerla de derecha a izquierda permitiendo asi invertir toda la cadena
+for (int i = text.Length - 1; i >= 0; i--)
+{
+    //Concatenamos el resultado para poder visualizar la cadena ya invertida.
+    rest += text[i];
+}
+
+Console.WriteLine(rest);
+
